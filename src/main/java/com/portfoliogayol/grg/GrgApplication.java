4b9980fbe6a0118822c36d927e0eaa112dc00eea
Gabriel Gayol <gabriel.gayol@gmail.com>
2023-05-11 10:19:49 -0300

    Iniciado proyecto

---
+++ src/main/java/com/portfoliogayol/grg/GrgApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliogayol.grg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfoliogayol/grg/GrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfoliogayol.grg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
